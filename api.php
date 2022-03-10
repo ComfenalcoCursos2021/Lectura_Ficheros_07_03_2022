5863cf56dbc84e40a769644556c953fd267dd520
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-09 20:33:23 -0500

    Leer JSON en el servidor

--- api.php
+++ api.php
@@ -1,7 +1,13 @@
 <?php
   header("Content-Type: application/json");
-  $datos = file_get_contents("datos.json");
+  $config = (array) [
+    "http" => (array) [
+      'method' => "GET"
+    ]
+  ];
+  $conversion = stream_context_create($config);
+  $datos = file_get_contents("https://comfenalcocursos2021.github.io/Lectura_Ficheros_07_03_2022/datos.json",false, $conversion);
   $decodificar = json_decode($datos);
 
-  var_dump($decodificar->infomacion);
+  var_dump($decodificar);
 ?>
