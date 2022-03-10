346ac87427306995eace99eed5b055c3ac76ac27
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-09 20:23:46 -0500

    Lectura de JSON local

--- api.php
+++ api.php
@@ -1,35 +1,7 @@
 <?php
   header("Content-Type: application/json");
-  
-  $data = (object) [];
-  $data->nombre = (string) "Miguel Angel Castro Escamilla";
-  $data->edad = (int) 24;
-  $data->altura = (float) 1.63;
-  $data->profesor = (bool) true;
-  $data->pasatiempos = (array) [
-    (string) "Cocino",
-    (string) "Programo"
-  ];
-  $data->infomacion = (array) [
-    (object) [
-      "Nombre" => (string) "Coco", 
-      "Especie" => (string) "Canino"
-    ],
-    (object) [
-      "Nombre" => (string) "Neko", 
-      "Especie" => (string) "Gato",
-      "Raza" => (bool) true
-    ],
-    (object) [
-      "Nombre" => (string) "pedro", 
-      "Especie" => (string) "Raton"
-    ],
-  ];
+  $datos = file_get_contents("datos.json");
+  $decodificar = json_decode($datos);
 
-  $codificar = json_encode($data, JSON_PRETTY_PRINT);
-
-
-  $file = fopen("datos.json", "w+");
-  fwrite($file, $codificar);
-  fclose($file);
+  var_dump($decodificar->infomacion);
 ?>
